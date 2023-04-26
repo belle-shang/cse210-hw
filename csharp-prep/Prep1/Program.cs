824a0fc11ae1fecd9687af583b9a47da64bbf438
belle-shang <belle3322@gmail.com>
2023-04-26 17:22:51 +0800

    name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,6 +5,12 @@ class Program
     static void Main(string[] args)
     {
         // This is prep 1
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine ("Hello Prep1 World!");
+        Console.Write ("What is your first name? ");
+        string first = Console.ReadLine ();
+        Console.Write ("What is your last name? ");
+        string last = Console.ReadLine ();
+        Console.WriteLine ("");
+        Console.Write ($"Your name is {last}, {first} {last}.");
     }
 }
