c099b71385c9cd6826d71a2b379ab94b3451c7f5
belle-shang <belle3322@gmail.com>
2023-04-26 17:38:51 +0800

    grade

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,60 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main (string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine ("Hello Prep2 World!");
+        Console.Write ("What is your grade percentage? ");
+        string answer = Console.ReadLine ();
+        int percent = int.Parse (answer);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        int plus = percent % 10;
+        string sign = "";
+        if (plus >= 7)
+        {
+            sign = "+";
+        }
+        else if (plus < 3)
+        {
+            sign = "-";
+        }
+        if (letter == "A" || letter =="F")
+        {
+            sign = "";
+        }
+
+        Console.WriteLine ($"Your grade is: {letter}{sign}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine ("You passed!");
+        }
+        else
+        {
+            Console.WriteLine ("You failed!");
+        }
     }
 }
