cbe0b6ce9a5b99363fd23dc55cc998f678f86259
belle-shang <belle3322@gmail.com>
2023-04-26 18:24:55 +0800

    number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,43 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main (string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int times = 0;
+        string answer = "";
+        int guess = -1;
+        Console.WriteLine ("Hello Prep3 World!");
+        
+        do
+        {
+            Console.Write ("What is the magic number? ");
+            int magic = int.Parse (Console.ReadLine ());
+            times = 0;
+
+        //Random randomGenerator = new Random();
+        //int magic = randomGenerator.Next(1, 101);
+            while (guess != magic)
+            {
+                Console.Write ("What is your guess? ");
+                guess = int.Parse (Console.ReadLine());
+                times++;
+
+                if (magic > guess)
+                {
+                    Console.WriteLine ("Higher");
+                }
+                else if (magic < guess)
+                {
+                    Console.WriteLine ("Lower");
+                }
+                else
+                {
+                    Console.WriteLine ("You guessed it!");
+                }
+            }
+            Console.WriteLine ($"You've guessed {times} times.");
+            Console.Write ("Would you like to play again? (yes/no) ");
+            answer = Console.ReadLine ();  
+        } while (answer == "yes");
     }
 }
